a10898d9ce3c7fa9bd822c45f83ad102555e3c29
Salvatore Dininni <s.dininni@yahoo.com>
2022-04-28 18:51:38 +0200

    map implementations

--- bier_app/src/App.js
+++ bier_app/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import './App.css';
 
 import {
@@ -12,6 +12,7 @@ import mapStyles from './mapStyles';
 // import "reach/combobox/styles.css";
 
 const libraries = ['places'];
+
 const mapContainerStyle = {
   width: '100vw',
   height: '100vh',
@@ -28,6 +29,7 @@ const options = {
   disableDefaultUI: true,
   zoomControll: true
 }
+
 let api_key = process.env.REACT_APP_API_GOOGLE_MAPS_API_KEY;
 
 function App() {
@@ -36,7 +38,21 @@ function App() {
     libraries,
   })
 
-  const [ markers, setMarkers] = useState([])
+  const [ markers, setMarkers] = useState([]);
+  const [ selected, setSelected ] = useState(null);
+  const onMapClick = useCallback((event) => {
+    setMarkers(current => [...current, {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+      time: new Date(),
+
+    }])
+  }, []);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  })
 
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Error loading maps";
@@ -49,23 +65,23 @@ function App() {
         zoom={11}
         center={center}
         options={options}
-        onClick={(event) => {
-          setMarkers(current => [...current, {
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng(),
-            time: new Date(),
-
-          }])
-        }}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
       {markers.map(marker => <Marker 
         key={marker.time.toISOString()} 
         position={{lat: marker.lat, lng: marker.lng}} 
         icon={
           {
-            url: 'beer-svg.svg'
+            url: 'beer-map.png',
+            scaledSize: new window.google.maps.Size(35, 35),
+            origin: new window.google.maps.Point(0,0),
+            anchor: new window.google.maps.Point(15, 15)
           }
         }
+        onClick={() => {
+          setSelected(marker);
+        }}
       />)}
       </GoogleMap>
     </>
